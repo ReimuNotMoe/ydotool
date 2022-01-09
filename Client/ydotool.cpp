8c3e2403017be7e13b7143dabcc09c6da7e6748b
Jerzy Drozdz <jerzy.drozdz@gmail.com>
2022-01-09 14:37:25 +0100

    Socket path is configurable in client

--- Client/ydotool.cpp
+++ Client/ydotool.cpp
@@ -1,20 +1,20 @@
 /*
-    This file is part of ydotool.
-    Copyright (C) 2018-2021 Reimu NotMoe <reimu@sudomaker.com>
+   This file is part of ydotool.
+   Copyright (C) 2018-2021 Reimu NotMoe <reimu@sudomaker.com>
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   */
 
 #include "ydotool.hpp"
 
@@ -23,8 +23,10 @@ using namespace uInputPlus;
 using namespace ydotool;
 
 static void ShowHelp(Tool::ToolManager *tool_mgr) {
-	std::cerr << "Usage: ydotool <cmd> <args>\n"
-		     "Available commands:\n";
+	std::cerr << "Usage: ydotool <opts> <cmd> <args>\n"
+		"Options:\n"
+		"  --socket-path\tPath to daemon socket, default: /tmp.ydotool_socket\n\n"
+		"Available commands:\n";
 
 	for (auto &it : tool_mgr->tools) {
 		std::cerr << "  " << it.first << std::endl;
@@ -33,21 +35,55 @@ static void ShowHelp(Tool::ToolManager *tool_mgr) {
 }
 
 int main(int argc, char **argv) {
-	std::unique_ptr<uInput> uinput_;
 
-	const char path_socket[] = "/tmp/.ydotool_socket";
+	cxxopts::Options options("ydotool", "ydotool client");
+
+	options
+		.allow_unrecognised_options()
+		.add_options()
+		("socket-path", "Socket path", cxxopts::value<std::string>()->default_value("/tmp/.ydotool_socket"))
+		;
+
+	std::string cfg_socket_path;
+	std::unique_ptr<uInput> uinput_;
+	Tool::ToolManager tool_mgr(uinput_.get());
+	std::vector<char *> commands;
+
+	try {
+		auto cmd = options.parse(argc, argv);
+		for (auto unmatched: cmd.unmatched()) {
+			commands.push_back(strdup(unmatched.c_str()));
+		}
+
+		cfg_socket_path = cmd["socket-path"].as<std::string>();
+
+	} catch (std::exception &e) {
+		std::cout << "Ooooops! " << e.what() << "\n";
+		std::cout << options.help();
+		return 0;
+	}
+
+	if (commands.size() < 1) {
+		ShowHelp(&tool_mgr);
+		exit(1);
+	}
+
+	if (strncmp(commands[0], "-h", 2) == 0 || strncmp(commands[0], "--h", 3) == 0 || strcmp(commands[0], "help") == 0) {
+		ShowHelp(&tool_mgr);
+		exit(1);
+	}
 
 	Socket<AddressFamily::Unix, SocketType::Datagram> socket_;
 
 	try {
 		socket_.create();
-		if (!socket_.connect({path_socket})) {
+		if (!socket_.connect(cfg_socket_path)) {
 			throw std::system_error(errno, std::system_category(), "failed to connect to socket");
 		}
 		uinput_ = std::make_unique<uInput>([&](uint16_t type, uint16_t code, int32_t val){
-			uInputRawData buf{type, code, val};
-			socket_.send(&buf, sizeof(buf));
-		});
+				uInputRawData buf{type, code, val};
+				socket_.send(&buf, sizeof(buf));
+				});
 		std::cerr << "ydotool: notice: Using ydotoold backend\n";
 	} catch (std::exception &e) {
 		std::cerr << "ydotool: notice: unable to access ydotoold backend: " << e.what() << "\n";
@@ -56,36 +92,22 @@ int main(int argc, char **argv) {
 		uinput_ = std::make_unique<uInput>(uInputSetup({"ydotool virtual device"}));
 	}
 
-	Tool::ToolManager tool_mgr(uinput_.get());
 
-	if (argc < 2) {
-		ShowHelp(&tool_mgr);
-		exit(1);
-	}
-
-	if (strncmp(argv[1], "-h", 2) == 0 || strncmp(argv[1], "--h", 3) == 0 || strcmp(argv[1], "help") == 0) {
-		ShowHelp(&tool_mgr);
-		exit(1);
-	}
 
 	std::vector<char *> splitted_argv;
 
-	for (int i=1; ; i++) {
-		if (!argv[i]) {
-			if (!splitted_argv.empty()) {
-				tool_mgr.run_tool(splitted_argv.size(), splitted_argv.data());
-			}
-			break;
-		}
-
-		if (0 == strcmp(argv[i], ",")) {
+	for (int i=0; i < commands.size(); i++) {
+		if (0 == strcmp(commands[i], ",")) {
 			if (!splitted_argv.empty()) {
 				tool_mgr.run_tool(splitted_argv.size(), splitted_argv.data());
 				splitted_argv.clear();
 			}
 		} else {
-			splitted_argv.emplace_back(argv[i]);
+			splitted_argv.emplace_back(commands[i]);
 		}
+	}
+	if (!splitted_argv.empty()) {
+		tool_mgr.run_tool(splitted_argv.size(), splitted_argv.data());
 	}
 
 	return 0;
